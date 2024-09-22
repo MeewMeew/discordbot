fc75e760bdbc5c4d0930bd2991d05b27194b3555
Nguyễn Mậu Minh <mauminh.nguyen@gmail.com>
2024-09-22 19:35:27 +0700

    chore: Change log scope in messageCreate

--- src/events/client/message.ts
+++ src/events/client/message.ts
@@ -4,9 +4,9 @@ import { Signale } from "signale"
 import config from "../../../config.json"
 import { buildEmbed } from "../../utils"
 
-export const name = "messageCreate"
+export const name = "message"
 export const run = (client: DisTubeClient) => {
-  const log = new Signale({ scope: "Message Create" })
+  const log = new Signale({ scope: "Message" })
   return async (message: Message) => {
     if (message.author.bot) return
     if (!message.content.startsWith(config.prefix)) return
